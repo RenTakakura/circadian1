985d7e319645abec478c2e9e59e2a4e6179bdc46
Ren <stun3perf2phys8@gmail.com>
2018-09-29 21:27:53 +0900

    trans

---
+++ 2step/2step_trans.c
@@ -0,0 +1,67 @@
+//2step model
+//phi output (for animation)
+
+#include <stdio.h>
+#include <math.h>
+
+#define k     -4.50
+
+//def external force
+double fxa(double xa,double xex){
+  return    k * sin(xa - xex);
+}
+
+int main(void){
+  
+  //def variables
+  int a, step, maxstep =25000;
+  double t, xa, xa0, va, va0, xex, kxa1, kxa2, kxa3, kxa4,v,dt = 1.0/500.0;
+
+  //open file  
+  FILE *fp;
+  fp = fopen("12sample_4.5_trans.dat","w");
+
+  //def variables
+  //xa =  M_PI*(2.0/3.0);
+  va =  2.0*M_PI*(12.0/24.0);
+  xex = 0.0;
+
+for(a=0; a<10; a++){
+
+  xa=2*M_PI*a/10;
+
+  //time development
+  for(step=0; step<maxstep; step++){
+    
+    xa0 = xa;
+    
+    t=dt*step;
+    
+    if(fmod(t,1.0)<=0.5){
+      
+      xex  = M_PI/2.0;
+      
+    }else{
+      
+      xex  =3.0* M_PI/2.0;
+      
+    }
+
+
+    //RungeKutta
+    kxa1 =   fxa(xa,xex)*dt;
+    kxa2 =   fxa(xa + kxa1/2,xex)*dt;
+    kxa3 =   fxa(xa + kxa2/2,xex)*dt;
+    kxa4 =   fxa(xa + kxa3,xex)*dt;
+    v =   (kxa1 + 2*kxa2 + 2*kxa3 + kxa4)/6 + va*dt;
+    xa =    xa + v;
+    
+    
+    
+    fprintf(fp,"%lf %lf %lf %lf \n", step*dt,sin(xex),sin(xa),v/dt);
+    
+  }
+
+}
+  
+}
---
+++ er/er_trans.c
@@ -0,0 +1,59 @@
+//er model
+//phi output (for animation)
+
+#include <stdio.h>
+#include <math.h>
+
+#define k     -2.0*M_PI/6.0
+
+//def external force
+double fxa(double xa,double xex){
+  return    k * sin(xa - xex);
+}
+
+int main(void){
+  
+  //def variables
+  int step, maxstep =10000, a;
+  double t, xa, xa0, va, va0, xex, kxa1, kxa2, kxa3, kxa4,v,dt = 1.0/500.0;
+
+  //open file  
+  FILE *fp;
+  fp = fopen("36er_0.5cri_trans.dat","w");
+
+  //def variables
+  //xa =  M_PI*(2.0/3.0);
+  va =  2.0*M_PI*(24.0/36.0);
+  xex = 0.0;
+
+for(a=0; a<10; a++){
+
+    xa = M_PI*2*a/10.0;
+
+  //time development
+  for(step=0; step<maxstep; step++){
+    
+    xa0 = xa;
+    
+    t=dt*step;
+    
+    xex = 2*M_PI*t;
+
+
+    //RungeKutta
+    kxa1 =   fxa(xa,xex)*dt;
+    kxa2 =   fxa(xa + kxa1/2,xex)*dt;
+    kxa3 =   fxa(xa + kxa2/2,xex)*dt;
+    kxa4 =   fxa(xa + kxa3,xex)*dt;
+    v =   (kxa1 + 2*kxa2 + 2*kxa3 + kxa4)/6 + va*dt;
+    xa =    xa + v;
+    
+    
+    
+    fprintf(fp,"%lf %lf %lf \n", step*dt,sin(xex),sin(xa));
+    
+  }
+
+}
+  
+}
